fc902d407fabeff1e0247f393f759399f9ee9a9a
JuanBunes <franciscobunes@rocketmail.com>
2019-09-12 11:41:07 -0300

    Rompi Todo

--- src/aliados/Guerrero.java
+++ src/aliados/Guerrero.java
@@ -6,14 +6,21 @@ import juego.*;
 public class Guerrero extends Aliado {
 	public Guerrero() {
 		super(40,1,500);
-		this.imagenes[0] = new ImageIcon(this.getClass().getResource("/sprites/Guerrero.png"));
+		
+		this.imagenes[0] = new ImageIcon("//Sprites//Guerrero.png");
+		
 		this.grafico = new JLabel(imagenes[0]);
+		this.grafico.setIcon(imagenes[0]);
 		this.grafico.setBounds(100, 100, tamañoAncho, tamañoLargo);
 	}
 	
 	public JLabel getGrafico(){
 		
 		return this.grafico;
+	}
+	
+	public ImageIcon getImage() {
+		return this.imagenes[0];
 	}
 	
 	public int getAncho() {
--- src/juego/Elemento.java
+++ src/juego/Elemento.java
@@ -1,17 +1,18 @@
 package juego;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public abstract class Elemento {
 	protected int vida;
 	protected final int tamañoAncho=32;
 	protected final int tamañoLargo=32;
-	protected Icon imagenes[];
+	protected ImageIcon imagenes[];
 	protected JLabel grafico;
 	
 	public Elemento(int v) {
 		vida=v;
-		imagenes=new Icon[1];
+		imagenes=new ImageIcon[1];
 		
 	}
 
