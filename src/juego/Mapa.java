2efadb16ec89fc627ffc9e62818c2c7c845f0e7a
MatiasPericolo <matiaspericolo@gmail.com>
2019-09-24 15:46:39 -0300

    Clase Mapa

--- src/juego/Mapa.java
+++ src/juego/Mapa.java
@@ -2,16 +2,23 @@ package juego;
 
 public final class Mapa {
 	protected Celda[][] cuadrilla;
+	protected final int largo=10;
+	protected final int ancho=6;
+	private static final Mapa mapa=new Mapa();
 	
-	public Mapa() {
-		cuadrilla=(Celda[][])new Celda[6][10];  //Arreglo[filas][columnas]
-		for(int i=0;i<6;i++) {
-			for(int j=0;j<10;j++) {
+	private Mapa() {
+		cuadrilla=(Celda[][])new Celda[ancho][largo];  //Arreglo[filas][columnas]
+		for(int i=0;i<ancho;i++) {
+			for(int j=0;j<largo;j++) {
 				cuadrilla[i][j]=new Celda(i,j);
 			}
 				
 		}
 		
+	}
+	
+	public static Mapa getMapa() {
+		return mapa;
 	}
 	
 	public Celda obtenerCelda(int x, int y) {
