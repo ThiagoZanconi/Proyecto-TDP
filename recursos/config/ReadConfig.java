817125819ff73b41abfb3e8ca83925cfbbad0218
JuanBunes <franciscobunes@rocketmail.com>
2019-10-13 15:53:44 -0300

    config.properties

---
+++ recursos/config/ReadConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import java.io.IOException;
+
+public class ReadConfig {
+	 
+	public static void main(String[] args) throws IOException {
+		GetPropertyValues properties = new GetPropertyValues();
+		properties.getPropValues();
+	}
+}
