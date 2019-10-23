1aa35835146a3f6a179fc568d7177582bb049d88
JuanBunes <franciscobunes@rocketmail.com>
2019-10-23 17:37:18 -0300

    asf

--- src/enemigos/Invocador.java
+++ src/enemigos/Invocador.java
@@ -10,7 +10,7 @@ public class Invocador extends Enemigo {
 		super(10,100,100,100);
 		enMovimiento=true;
 		this.imagenes[0] = new JLabel();
-		imagenes[0].setIcon(new ImageIcon("Sprites\\Invocador\\InvocadorSummon.gif"));
+		imagenes[0].setIcon(new ImageIcon("Sprites\\Invocador\\InvocadorWalk.gif"));
 		this.graficoActual = new JLabel();
 		this.graficoActual.setIcon(imagenes[0].getIcon());
 		this.graficoActual.setBounds(100, 100, 50, 100);
