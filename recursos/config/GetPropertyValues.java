0026550bf8e55d0509c3652ff65898057677128c
JuanBunes <franciscobunes@rocketmail.com>
2019-10-31 17:06:32 -0300

    Revert "."
    This reverts commit 09f532cdd99b360e61a14adeee2609aee8de574a.

---
+++ recursos/config/GetPropertyValues.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+public class GetPropertyValues {
+	
+	String toReturn="";
+	InputStream inputStream;
+	
+	public String getPropValues() throws IOException {
+		
+		
+		
+		
+		
+		
+		return toReturn;
+	}
+
+}
