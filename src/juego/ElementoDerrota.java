c41f62f6ac900862935260b0a7c80d68d3f53eac
JuanBunes <franciscobunes@rocketmail.com>
2019-11-01 23:52:15 -0300

    asf

--- src/juego/ElementoDerrota.java
+++ src/juego/ElementoDerrota.java
@@ -1,6 +1,13 @@
 package juego;
 
 import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 import visitor.Visitor;
 import visitor.VisitorElementoDerrota;
 
@@ -25,6 +32,19 @@ public class ElementoDerrota extends Elemento {
 	
 	public void derrota() {
 		//Abrir ventana Con cartel que muestre la derrota y un boton aceptar para volver al menu principal
+		JFrame derrFrame=new JFrame("Derrota");
+		JPanel derrPanel=new JPanel();
+		JLabel derr=new JLabel(new ImageIcon("Sprites\\derrota.png"));
+		derrFrame.setContentPane(derrPanel);
+		derrFrame.setBounds(700,400,405,210);
+		derrFrame.setVisible(true);
+		derrFrame.setResizable(false);
+		derrPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		derrPanel.setLayout(null);
+		
+		derrPanel.add(derr);
+		derr.setVisible(true);
+		
 	}
 
 }
