a2e2de3acab645408eedf893aa529a0ff705859b
JuanBunes <franciscobunes@rocketmail.com>
2019-11-02 03:08:49 -0300

    derrota

--- src/juego/ElementoDerrota.java
+++ src/juego/ElementoDerrota.java
@@ -3,6 +3,7 @@ package juego;
 import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,20 +31,41 @@ public class ElementoDerrota extends Elemento {
 	}
 	
 	public void derrota() {
+		
 		adaptador.terminarJuego();
-		//Abrir ventana Con cartel que muestre la derrota y un boton aceptar para volver al menu principal
+		
+		JButton derrButton=new JButton(new ImageIcon("Sprites\\button_volver-al-menu.png"));
 		JFrame derrFrame=new JFrame("Derrota");
 		JPanel derrPanel=new JPanel();
 		JLabel derr=new JLabel(new ImageIcon("Sprites\\derrota.png"));
-		derrFrame.setContentPane(derrPanel);
+		JLabel derr2=new JLabel(new ImageIcon("Sprites\\cred2.png"));
+		
 		derrFrame.setBounds(700,400,405,210);
 		derrFrame.setVisible(true);
 		derrFrame.setResizable(false);
+		derrFrame.setContentPane(derrPanel);
+		
+		
+		
+		
+		derrButton.setBounds(115, 110, 170, 53);
+		
 		derrPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		derrPanel.setLayout(null);
-
+		
 		derrPanel.add(derr);
+		derr.setBounds(0, 0, 402, 105);
 		derr.setVisible(true);
+		
+		derr2.setBounds(0, -100, 402, 400);
+		derr2.setVisible(true);
+		derrPanel.add(derrButton);
+		derrPanel.add(derr2);
+		
+		
+		
+				
+		
 		
 	}
 
